029ecb9472a9327dcb0cdde4c01f3c967200b54d
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-11-22 02:53:52 +0700

    Add load database config from config.json file

--- CrawlData/src/main/java/config/LoadConfigOperator.java
+++ CrawlData/src/main/java/config/LoadConfigOperator.java
@@ -1,23 +1,72 @@
 package config;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import enums.CrawlType;
+import enums.LogLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import models.DataSource;
+import models.ProcessDetail;
+import services.ControlService;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.sql.Timestamp;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class LoadConfigOperator {
+    private String configControl = "/config/config.json";
     private String configPath = "/config/configLoader.jar";
+    private String logMessage = "";
+    private ControlService service = new ControlService();
+
+    public DatabaseConnection loadDatabaseConfig() {
+        try {
+            File configFile = new File(configControl);
+            if (!configFile.exists()) {
+                return null;
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(configFile)));
+            StringBuilder jsonContent = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonContent.append(line);
+            }
+            br.close();
+            JsonObject configJson = JsonParser.parseString(jsonContent.toString()).getAsJsonObject();
+
+            String name = configJson.get("name").getAsString();
+            String type = configJson.get("type").getAsString();
+            String host = configJson.get("host").getAsString();
+            String port = configJson.get("port").getAsString();
+            String username = configJson.get("username").getAsString();
+            String password = configJson.get("password").getAsString();
+            String characterEncoding = configJson.get("characterEncoding").getAsString();
+
+            DatabaseConnection databaseConnection;
+            databaseConnection = new DatabaseConnection();
+            databaseConnection.setName(name);
+            databaseConnection.setType(type);
+            databaseConnection.setHost(host);
+            databaseConnection.setPort(port);
+            databaseConnection.setUsername(username);
+            databaseConnection.setPassword(password);
+            databaseConnection.setCharacterEncoding(characterEncoding);
+            return databaseConnection;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
     public DataSource loadConfig() {
         try {
