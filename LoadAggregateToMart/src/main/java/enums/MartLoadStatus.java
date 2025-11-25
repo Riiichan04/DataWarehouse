2882fd613025cb8aea00a5460990f42f98a1294f
Quang.Tran <22130230@st.hcmuaf.edu.vn>
2025-11-25 18:09:26 +0700

    aggregate-loader-business-add: thêm enums (MartLoadStatus, MartLogLevel)

---
+++ LoadAggregateToMart/src/main/java/enums/MartLoadStatus.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum MartLoadStatus {
+    SUCCESS,       // load bảng thành công
+    FAILED,        // load bảng thất bại
+    SKIPPED       // bảng không có dữ liệu nên bỏ qua
+}
